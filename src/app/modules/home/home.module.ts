edba85b2d6aa5b15572c2decad7bebd9947b591e
Gerardo <gerardopapaleco@gmail.com>
2022-03-02 21:24:24 -0400

    feat: drag and drop testing

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { HomeRoutingModule } from './home-routing.module';
-
-import { HomeComponent } from './home.component';
+import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-
 import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective],
-  imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  imports: [CommonModule, TranslateModule, FormsModule, DragDropModule],
+  exports: [TranslateModule, WebviewDirective, FormsModule, DragDropModule]
 })
 export class SharedModule {}

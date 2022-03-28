c7fb2791ec30e0b41bee8d6bebff60f732d7ed41
Gerardo <gerardopapaleco@gmail.com>
2022-03-28 19:54:43 -0400

    feat: Added input for  element's top - left styles
    Added inputs for manipulating selected element position

--- src/app/home/styles-bar/styles-bar.component.ts
+++ src/app/home/styles-bar/styles-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { SelectedElementsService } from '../../core/services/selected-elements/selected-elements.service';
 
 @Component({
@@ -8,17 +8,52 @@ import { SelectedElementsService } from '../../core/services/selected-elements/s
 })
 export class StylesBarComponent implements OnInit {
   selectedElement: HTMLElement;
+  canvasRect: DOMRect;
+  elementAtt = {
+    top: 0,
+    left: 0,
+  };
 
-  constructor(private selectedService: SelectedElementsService) {
+  constructor(
+    private selectedService: SelectedElementsService,
+    private renderer: Renderer2
+  ) {
     this.selectedService.currentSelected.subscribe((value) => {
       this.selectedElement = value[value.length - 1];
-      console.log(this.selectedElement);
+      if (!this.isElementSelected()) {
+        return;
+      }
+
+      this.canvasRect =
+        this.selectedElement.parentElement.getBoundingClientRect();
+
+      const elementTop = parseInt(this.selectedElement.style.top, 10);
+      this.elementAtt.top = elementTop - this.canvasRect.top;
+
+      const elementLeft = parseInt(this.selectedElement.style.left, 10);
+      this.elementAtt.left = elementLeft - this.canvasRect.left;
     });
   }
 
   ngOnInit(): void {}
 
-  isElementSelected() {
+  isElementSelected(): boolean {
     return this.selectedElement !== undefined;
+  }
+
+  changePos({ target: input }: any) {
+    // Get input data
+    const posValue = parseInt(input.value, 10);
+    // Axis depending of the input name (top | left)
+    const axis = input.name;
+
+    // Parent value for top or left
+    const canvasValue = this.canvasRect[axis];
+
+    this.renderer.setStyle(
+      this.selectedElement,
+      axis,
+      `${canvasValue + posValue}px`
+    );
   }
 }

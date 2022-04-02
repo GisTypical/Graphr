c74e026a478df1c50872499add90af0e5d7765dd
Nathalie Zambrano <nathalievzm@hotmail.com>
2022-04-02 18:49:27 -0400

    rebasing changes

--- src/app/home/toolbar/toolbar.component.ts
+++ src/app/home/toolbar/toolbar.component.ts
@@ -8,7 +8,7 @@ import { ElectronService } from '../../core/services';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-  @ViewChild('projectButton') projectButton: ElementRef;
+  @ViewChild('projectButton') public projectButton: ElementRef;
   @ViewChild('projectMenu') projectMenu: ElementRef;
   @ViewChild('moveButton') moveButton: ElementRef;
   @ViewChild('moveMenu') moveMenu: ElementRef;
@@ -17,7 +17,10 @@ export class ToolbarComponent implements OnInit {
   isMoveMenuOpen = false;
   isElectron = true;
 
-  constructor(private renderer: Renderer2, private electronService: ElectronService) {
+  constructor(
+    private renderer: Renderer2,
+    private electronService: ElectronService
+  ) {
     this.renderer.listen('window', 'click', (e: Event) => {
       if (this.projectButton && this.projectMenu) {
         if (!this.projectButton.nativeElement.contains(e.target) && !this.projectMenu.nativeElement.contains(e.target)) {
@@ -41,6 +44,15 @@ export class ToolbarComponent implements OnInit {
 
   toggleMoveMenu(): void {
     this.isMoveMenuOpen = !this.isMoveMenuOpen;
+  }
+
+  generatePageFiles(): void {
+    const canvas = document.getElementsByTagName('app-canvas')[0].children;
+    const canvasChildren = Array.from(canvas);
+
+    for (const element of canvasChildren) {
+      console.log((element as HTMLElement).style);
+    }
   }
 
 }
